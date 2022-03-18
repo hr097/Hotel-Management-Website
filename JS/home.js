14daf000042bd4ba6c5baa3d7294debad0871a61
hr097 <harshilramani9777@gmail.com>
2022-03-18 14:28:46 +0530

    75% work done

--- JS/home.js
+++ JS/home.js
@@ -28,7 +28,7 @@ function navbar_open() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 3000); // Change image every 2 seconds
+    setTimeout(carousel, 4000); // Change image every 2 seconds
   }  
 
   
